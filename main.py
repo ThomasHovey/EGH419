8b41a5750dc61f4d4e7354e9a33051cdb24166dd
Thomas Hovey <thomas.hovey.1997@gmail.com>
2018-05-16 00:06:30 +1000

    Update main.py test

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import cv2
 import numpy as np
 import dewarp_lib
+from picamera.array import PiRGBArray
+from picamera import PiCamera 
+import time
 
-im_1 =cv2.imread('img4.jpg', 0)
-img = dewarp_lib.unwarp(im_1)
-print img.size
-cv2.imshow(img)
-cv2.waitKey(0)
+# Setup PiCamera
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+
+time.sleep(0.1)
+
+def get_img():
+	camera.capture(rawCapture, format='gray')
+	img = rawCapture.array
+
+	return img
+
+
+while (1):
+	img = get_img()
+	cv2.imshow(img)
+	cv2.waitKey(0)
+
+	
