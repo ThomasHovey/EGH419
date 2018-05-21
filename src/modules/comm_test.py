e89cad7a0f2db70f32067da8530d9ba622420c5c
William1993 <William1993@users.noreply.github.com>
2018-05-21 15:50:47 +1000

    test

--- src/modules/comm_test.py
+++ src/modules/comm_test.py
@@ -5,16 +5,38 @@ from classes.Pose import Pose
 import comm
 import matplotlib.pyplot as plt
 
-READ_ENCODERS = "<ECD:>"
+state.LeftMotorSpeed = 140
+state.RightMotorSpeed = -140
 
+READ_ENCODERS = "<ECD:>"
+READ_IMU = "<IMU:>"
+READ_COMPASS = "<MAG:>"
+READ_ALL = "<ALL:>"
+SENT_SPEED = "<MoSp:" + str(state.LeftMotorSpeed) + "," + str(state.RightMotorSpeed) + ">"
 
 # Setup serial 
 comm.Serial_init()
 
 # Create a new 
 state = State()
+time.sleep(2)
+
+# Update encoders
+comm.SerialSendCommand(state,READ_ENCODERS)
+time.sleep(5)
+
+# Update IMU
+comm.SerialSendCommand(state,READ_IMU)
+time.sleep(5)
+
+# Update Compass
+comm.SerialSendCommand(state,READ_COMPASS)
+time.sleep(5)
+
+# Update all encoders, IMU, Compass
+comm.SerialSendCommand(state,READ_ALL)
+time.sleep(5)
 
 # Update motorspeed
-state.LeftMotorSpeed = 140
-state.RightMotorSpeed = -140
-comm.SerialSendCommand(state,READ_ENCODERS)
+comm.SerialSendCommand(state,SENT_SPEED)
+time.sleep(5)
