cfd1edb724d95d0b22727e1c49f6031ffaee58bd
Thomas Hovey <thomas.hovey.1997@gmail.com>
2018-05-21 15:19:35 +1000

    Update Class

---
+++ src/modules/classes/Compass.py
@@ -0,0 +1,6 @@
+class Compass:
+	def __init__(self,x_mag,y_mag,z_mag):
+		self.x_mag = x_mag
+		self.y_mag = y_mag
+		self.z_mag = z_mag
+		
--- src/modules/classes/IMU.py
+++ src/modules/classes/IMU.py
@@ -1,7 +1,9 @@
 class IMU:
 
-	def __init__(self,x_accel,y_accel,z_gyro,compass):
+	def __init__(self,x_accel,y_accel,z_accel,x_gyro,y_gyro,z_gyro,compass):
 		self.x_accel = x_accel
 		self.y_accel = y_accel
+		self.z_accel = z_accel
+		self.x_gyro = x_gyro
+		self.y_gyro = y_gyro
 		self.z_gyro = z_gyro
-		self.compass = compass
