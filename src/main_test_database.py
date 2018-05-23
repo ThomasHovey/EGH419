4f91bc3136e47d3e4b29f6205072a5ffaad0e6d6
Thomas Hovey <thomas.hovey.1997@gmail.com>
2018-05-23 10:25:10 +1000

    Database Test

--- src/main_test_database.py
+++ src/main_test_database.py
@@ -15,9 +15,9 @@ time.sleep(0.1)
 
 	
 # Build database
-database = build_database()
+database = place_recog.build_database()
 
-database_new = load_database()
+database_new = place_recog.load_database()
 
 if database == database_new:
 	print("Database loaded correctly\n")
@@ -25,17 +25,17 @@ else:
 	print("Database load failed\n")
 
 # Setup current location
-pose_current = class_info.Pose(1.5,0,0,0,0,0)
+pose_current = Pose(1.5,0,0,0,0,0)
 
 while 1:
 	# When ready capture an image
 	# key = raw_input("Enter any key to check (e or E to exit)")
 	# if key == 'E' or key == 'e':
 	# 	break
-	capture = get_image()
+	capture = place_recog.get_image()
 
 	# Image search for closest image
-	new_pose, error = find_location(capture, database, pose_current)
+	new_pose, error = place_recog.find_location(capture, database, pose_current)
 
 	# Print answer
 	print("Pos : " + str(new_pose.x) + " Error : " + str(error))
